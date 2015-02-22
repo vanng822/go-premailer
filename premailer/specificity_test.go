93c03a6a128057864f59e968ad993eb036c305fa
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-02-22 11:33:03 +0100

    Adding test for specificity sorting

--- premailer/specificity_test.go
+++ premailer/specificity_test.go
@@ -3,6 +3,7 @@ package premailer
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"sort"
 )
 
 func TestSpecificitySelectorType(t *testing.T) {
@@ -31,4 +32,65 @@ func TestSpecificitySelectorId(t *testing.T) {
 	spec := makeSpecificity(0, 3, 104, "#example")
 	expected := []int{0, 1, 0, 0, 0, 3, 104}
 	assert.Equal(t, expected, spec.importantOrders())
+}
+
+func TestSpecificitySort(t *testing.T) {
+	undertest := make([]*styleRule, 4)
+	for i := 0; i < 4; i++ {
+		undertest[i] = &styleRule{}
+	}
+	specificity0 := makeSpecificity(1, 2, 100, "table")
+	undertest[0].specificity = specificity0
+	specificity1 := makeSpecificity(1, 2, 102, "table.red")
+	undertest[1].specificity = specificity1
+	specificity2 := makeSpecificity(1, 3, 103, "span[lang~=\"en-us\"]")
+	undertest[2].specificity = specificity2
+	specificity3 := makeSpecificity(0, 3, 104, "#example")
+	undertest[3].specificity = specificity3
+	
+	// expected order 
+	/*
+	expected3 := []int{0, 1, 0, 0, 0, 3, 104}
+	expected0 := []int{1, 0, 0, 0, 1, 2, 100}
+	expected2 := []int{1, 0, 0, 1, 1, 3, 103}
+	expected1 := []int{1, 0, 1, 0, 1, 2, 102}
+	*/
+	sort.Sort(bySpecificity(undertest))
+	
+	assert.Equal(t, specificity3, undertest[0].specificity)
+	assert.Equal(t, specificity0, undertest[1].specificity)
+	assert.Equal(t, specificity2, undertest[2].specificity)
+	assert.Equal(t, specificity1, undertest[3].specificity)
+}
+
+func TestSpecificitySortRuleSetIndex(t *testing.T) {
+	undertest := make([]*styleRule, 2)
+	for i := 0; i < 2; i++ {
+		undertest[i] = &styleRule{}
+	}
+	specificity0 := makeSpecificity(1, 2, 102, "table")
+	undertest[0].specificity = specificity0
+	specificity1 := makeSpecificity(1, 1, 102, "table")
+	undertest[1].specificity = specificity1
+	
+	sort.Sort(bySpecificity(undertest))
+	
+	assert.Equal(t, specificity1, undertest[0].specificity)
+	assert.Equal(t, specificity0, undertest[1].specificity)
+}
+
+func TestSpecificitySortRuleIndex(t *testing.T) {
+	undertest := make([]*styleRule, 2)
+	for i := 0; i < 2; i++ {
+		undertest[i] = &styleRule{}
+	}
+	specificity0 := makeSpecificity(1, 1, 102, "table")
+	undertest[0].specificity = specificity0
+	specificity1 := makeSpecificity(1, 1, 100, "table")
+	undertest[1].specificity = specificity1
+	
+	sort.Sort(bySpecificity(undertest))
+	
+	assert.Equal(t, specificity1, undertest[0].specificity)
+	assert.Equal(t, specificity0, undertest[1].specificity)
 }
