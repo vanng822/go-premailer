05a543afedfae9a0be418ca4e01cd230594a7ff8
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-02-26 21:03:01 +0100

    Adding doc file

---
+++ premailer/doc.go
@@ -0,0 +1,21 @@
+// Package premailer is for inline styling
+// 
+//	import (
+//		"fmt"
+//		"github.com/vanng822/go-premailer/premailer"
+//		"log"
+//	)
+//	
+//	func main() {
+//		prem := premailer.NewPremailerFromFile(inputFile)
+//		html, err := prem.Transform()
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//		
+//		fmt.Println(html)
+//	}
+	
+package premailer
+
+
