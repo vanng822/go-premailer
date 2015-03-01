a13b28067fd10ac956e2f1e075ed336a786df26e
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-01 20:27:32 +0100

    Adding output example

--- premailer/doc.go
+++ premailer/doc.go
@@ -15,6 +15,33 @@
 //		
 //		fmt.Println(html)
 //	}
+//	// Input
+// 
+//	<html>
+//	<head>
+//	<title>Title</title>
+//	<style type="text/css">
+//		h1 { width: 300px; color:red; }
+//		strong { text-decoration:none; }
+//	</style>
+//	</head>
+//	<body>
+//		<h1>Hi!</h1>
+//		<p><strong>Yes!</strong></p>
+//	</body>
+//	</html>
+//	
+// // Output
+//
+//	<html>
+//	<head>
+//	<title>Title</title>
+//	</head>
+//	<body>
+//		<h1 style="color:red;width:300px" width="300">Hi!</h1>
+//		<p><strong style="text-decoration:none">Yes!</strong></p>
+//	</body>
+//	</html>
 package premailer
 
 
