4ae54dc0a3c2ac473391f2a1af375c7a970eb731
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2019-04-05 20:37:18 +0200

    support creating Premailer from bytes

---
+++ premailer/premailer_from_bytes_test.go
@@ -0,0 +1,51 @@
+package premailer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPremailerBasicHTMLBytes(t *testing.T) {
+	html := []byte(`<html>
+        <head>
+        <title>Title</title>
+        <style type="text/css">
+        h1 {
+        	width: 50px;
+        	color:red;
+        }
+        h2 {
+        	vertical-align: top;
+        }
+        h3 {
+		    text-align: right;
+		}
+        strong {
+        	text-decoration:none
+        }
+        div {
+        	background-color: green
+        }
+        </style>
+        </head>
+        <body>
+        <h1>Hi!</h1>
+        <h2>There</h2>
+        <h3>Hello</h3>
+        <p><strong>Yes!</strong></p>
+        <div>Green color</div>
+        </body>
+        </html>`)
+
+	p, err := NewPremailerFromBytes(html, nil)
+	assert.Nil(t, err)
+	result_html, err := p.Transform()
+	assert.Nil(t, err)
+
+	assert.Contains(t, result_html, "<h1 style=\"color:red;width:50px\" width=\"50\">Hi!</h1>")
+	assert.Contains(t, result_html, "<h2 style=\"vertical-align:top\" valign=\"top\">There</h2>")
+	assert.Contains(t, result_html, "<h3 style=\"text-align:right\" align=\"right\">Hello</h3>")
+	assert.Contains(t, result_html, "<p><strong style=\"text-decoration:none\">Yes!</strong></p>")
+	assert.Contains(t, result_html, "<div style=\"background-color:green\" bgcolor=\"green\">Green color</div>")
+}
