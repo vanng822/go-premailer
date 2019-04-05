9e5acafb5fcfa14aaae46b3a07ba380669236b46
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2019-04-05 21:11:46 +0200

    Use same test string

--- premailer/benchmark_test.go
+++ premailer/benchmark_test.go
@@ -1,54 +1,19 @@
 package premailer
 
 import (
+	"bytes"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func BenchmarkPremailerBasicHTMLBytes(b *testing.B) {
-	html := []byte(`<html>
-        <head>
-        <title>Title</title>
-        <style type="text/css">
-        h1 {
-        	width: 50px;
-        	color:red;
-        }
-        h2 {
-        	vertical-align: top;
-        }
-        h3 {
-		    text-align: right;
-		}
-        strong {
-        	text-decoration:none
-        }
-        div {
-        	background-color: green
-        }
-        </style>
-        </head>
-        <body>
-        <h1>Hi!</h1>
-        <h2>There</h2>
-        <h3>Hello</h3>
-        <p><strong>Yes!</strong></p>
-        <div>Green color</div>
-        </body>
-        </html>`)
+var (
+	testBenmarkBuf       *bytes.Buffer
+	testBenmarkHtmlBytes []byte
+	testBenmarkHtml      string
+)
 
-	for n := 0; n < b.N; n++ {
-		p, err := NewPremailerFromBytes(html, nil)
-		assert.Nil(b, err)
-		result_html, err := p.Transform()
-		assert.NotNil(b, result_html)
-		assert.Nil(b, err)
-	}
-}
-
-func BenchmarkPremailerBasicHTML(b *testing.B) {
-	html := `<html>
+func init() {
+	testBenmarkBuf = new(bytes.Buffer)
+	testString := `<html>
         <head>
         <title>Title</title>
         <style type="text/css">
@@ -78,53 +43,28 @@ func BenchmarkPremailerBasicHTML(b *testing.B) {
         <div>Green color</div>
         </body>
         </html>`
+	testBenmarkBuf.WriteString(testString)
+	testBenmarkHtml = testString
+	testBenmarkHtmlBytes = []byte(testString)
+}
 
+func BenchmarkPremailerBasicHTMLBytes(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		p, err := NewPremailerFromString(html, nil)
-		assert.Nil(b, err)
-		result_html, err := p.Transform()
-		assert.NotNil(b, result_html)
-		assert.Nil(b, err)
+		p, _ := NewPremailerFromBytes(testBenmarkHtmlBytes, nil)
+		p.Transform()
+	}
+}
+
+func BenchmarkPremailerBasicHTML(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		p, _ := NewPremailerFromString(testBenmarkHtml, nil)
+		p.Transform()
 	}
 }
 
 func BenchmarkPremailerBasicHTMLBytes2String(b *testing.B) {
-	html := []byte(`<html>
-        <head>
-        <title>Title</title>
-        <style type="text/css">
-        h1 {
-        	width: 50px;
-        	color:red;
-        }
-        h2 {
-        	vertical-align: top;
-        }
-        h3 {
-		    text-align: right;
-		}
-        strong {
-        	text-decoration:none
-        }
-        div {
-        	background-color: green
-        }
-        </style>
-        </head>
-        <body>
-        <h1>Hi!</h1>
-        <h2>There</h2>
-        <h3>Hello</h3>
-        <p><strong>Yes!</strong></p>
-        <div>Green color</div>
-        </body>
-        </html>`)
-
 	for n := 0; n < b.N; n++ {
-		p, err := NewPremailerFromString(string(html), nil)
-		assert.Nil(b, err)
-		result_html, err := p.Transform()
-		assert.NotNil(b, result_html)
-		assert.Nil(b, err)
+		p, _ := NewPremailerFromString(testBenmarkBuf.String(), nil)
+		p.Transform()
 	}
 }
